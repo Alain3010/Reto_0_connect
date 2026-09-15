a34770edddade052f63e7808eb8a74991ab706a8
Mercadouna <20alain.martinez05@gmail.com>
2026-09-15 13:36:16 +0200

    primer commit

---
+++ src/main/java/com/mycompany/reto_0_conecta/model/Genero.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto_0_conecta.model;
+
+public enum Genero {
+    TERROR, ACCION, COMEDIA
+}
