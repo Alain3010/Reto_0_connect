25e3ab8efacb66b0da78d367b6b4332aac2c64cb
Mercadouna <20alain.martinez05@gmail.com>
2026-09-16 08:32:53 +0200

    Main, Controlador y Utilidades

---
+++ src/main/java/com/mycompany/reto_0_conecta/util/MyObjectOutputStream.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto_0_conecta.util;
+
+/**
+ *
+ * @author 20ala
+ */
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class MyObjectOutputStream extends ObjectOutputStream{
+		 //Sobrescribimos el m�todo que crea la cabecera
+                 @Override
+		 protected void writeStreamHeader() throws IOException
+		 {
+			 	reset();
+		 }
+		 //Constructores
+		 public MyObjectOutputStream () throws IOException{
+			 super();
+		 }
+		 public MyObjectOutputStream(OutputStream out) throws IOException
+		 {
+			 super(out);
+		 }
+}
