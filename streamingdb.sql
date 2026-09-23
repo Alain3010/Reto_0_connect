1051f38af337b8f9a55bf14461badf077b2e4380
SrEstroncio038 <asolagurenanaut@gmail.com>
2026-09-23 08:57:28 +0200

    Add files via upload
    updated bp script

--- streamingdb.sql
+++ streamingdb.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF exists streamingdb;
 CREATE DATABASE IF NOT EXISTS streamingdb DEFAULT CHARACTER SET = 'utf8mb4' DEFAULT COLLATE 'utf8mb4_general_ci';
 
 USE streamingdb;
@@ -5,7 +6,7 @@ USE streamingdb;
 CREATE TABLE user (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone_num VARCHAR(50)
 );
@@ -23,7 +24,7 @@ CREATE TABLE watchlist (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     creationDate DATE NOT NULL,
-    mov_count INT NOT NULL DEFAULT 0,
+    mov_num INT NOT NULL DEFAULT 0,
     id_user INT NOT NULL
 );
 
@@ -33,34 +34,28 @@ CREATE TABLE watchlistMovie (
     PRIMARY KEY (id_watchlist, id_movie)
 );
 
-ALTER TABLE watchlist ADD CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES user(id) ON DELETE CASCADE;
-ALTER TABLE watchlistMovie ADD CONSTRAINT fk_watchlist FOREIGN KEY (id_watchlist) REFERENCES watchlist(id) ON DELETE CASCADE;
-ALTER TABLE watchlistMovie ADD CONSTRAINT fk_movie FOREIGN KEY (id_movie) REFERENCES movie(id) ON DELETE CASCADE;
+ALTER TABLE watchlist ADD CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES user(id);
+ALTER TABLE watchlistMovie ADD CONSTRAINT fk_watchlist FOREIGN KEY (id_watchlist) REFERENCES watchlist(id);
+ALTER TABLE watchlistMovie ADD CONSTRAINT fk_movie FOREIGN KEY (id_movie) REFERENCES movie(id);
 
-CREATE USER 'reto0'@'localhost' IDENTIFIED BY 'Reto0_Grupo4';
-
-GRANT DELETE, EXECUTE, INSERT, SELECT, SHOW VIEW, UPDATE ON streamingb.* TO 'reto0'@'localhost';
-
-FLUSH PRIVILEGES;
-
-INSERT INTO movie (id, title, director, genre, adults, route) VALUES
-(1, 'The Shining', 'Stanley Kubrick', 'HORROR', TRUE, '/movies/the-shining.jpg'),
-(2, 'It', 'Andy Muschietti', 'HORROR', TRUE, '/movies/it.jpg'),
-(3, 'The Conjuring', 'James Wan', 'HORROR', TRUE, '/movies/the-conjuring.jpg'),
-(4, 'A Nightmare on Elm Street', 'Wes Craven', 'HORROR', TRUE, '/movies/nightmare-on-elm-street.jpg'),
-(5, 'Scream', 'Wes Craven', 'HORROR', TRUE, '/movies/scream.jpg'),
-(6, 'The Dark Knight', 'Christopher Nolan', 'ACTION', TRUE, '/movies/the-dark-knight.jpg'),
-(7, 'Mad Max: Fury Road', 'George Miller', 'ACTION', TRUE, '/movies/mad-max-fury-road.jpg'),
-(8, 'Die Hard', 'John McTiernan', 'ACTION', TRUE, '/movies/die-hard.jpg'),
-(9, 'Gladiator', 'Ridley Scott', 'ACTION', TRUE, '/movies/gladiator.jpg'),
-(10, 'John Wick', 'Chad Stahelski', 'ACTION', TRUE, '/movies/john-wick.jpg'),
-(11, 'The Matrix', 'Lana Wachowski', 'ACTION', TRUE, '/movies/the-matrix.jpg'),
-(12, 'Terminator 2', 'James Cameron', 'ACTION', TRUE, '/movies/terminator-2.jpg'),
-(13, 'Mission: Impossible', 'Brian De Palma', 'ACTION', TRUE, '/movies/mission-impossible.jpg'),
-(14, 'The Mask', 'Chuck Russell', 'COMEDY', FALSE, '/movies/the-mask.jpg'),
-(15, 'Superbad', 'Greg Mottola', 'COMEDY', TRUE, '/movies/superbad.jpg'),
-(16, 'Groundhog Day', 'Harold Ramis', 'COMEDY', FALSE, '/movies/groundhog-day.jpg'),
-(17, 'Dumb and Dumber', 'Peter Farrelly', 'COMEDY', TRUE, '/movies/dumb-and-dumber.jpg'),
-(18, 'Home Alone', 'Chris Columbus', 'COMEDY', FALSE, '/movies/home-alone.jpg'),
-(19, 'The Hangover', 'Todd Phillips', 'COMEDY', TRUE, '/movies/the-hangover.jpg'),
-(20, 'Mrs. Doubtfire', 'Chris Columbus', 'COMEDY', FALSE, '/movies/mrs-doubtfire.jpg');
+INSERT INTO movie (title, director, genre, adults, route) VALUES
+('The Shining', 'Stanley Kubrick', 'HORROR', TRUE, '/movies/the-shining.jpg'),
+('It', 'Andy Muschietti', 'HORROR', TRUE, '/movies/it.jpg'),
+( 'The Conjuring', 'James Wan', 'HORROR', TRUE, '/movies/the-conjuring.jpg'),
+( 'A Nightmare on Elm Street', 'Wes Craven', 'HORROR', TRUE, '/movies/nightmare-on-elm-street.jpg'),
+( 'Scream', 'Wes Craven', 'HORROR', TRUE, '/movies/scream.jpg'),
+( 'The Dark Knight', 'Christopher Nolan', 'ACTION', TRUE, '/movies/the-dark-knight.jpg'),
+( 'Mad Max: Fury Road', 'George Miller', 'ACTION', TRUE, '/movies/mad-max-fury-road.jpg'),
+( 'Die Hard', 'John McTiernan', 'ACTION', TRUE, '/movies/die-hard.jpg'),
+( 'Gladiator', 'Ridley Scott', 'ACTION', TRUE, '/movies/gladiator.jpg'),
+( 'John Wick', 'Chad Stahelski', 'ACTION', TRUE, '/movies/john-wick.jpg'),
+( 'The Matrix', 'Lana Wachowski', 'ACTION', TRUE, '/movies/the-matrix.jpg'),
+( 'Terminator 2', 'James Cameron', 'ACTION', TRUE, '/movies/terminator-2.jpg'),
+( 'Mission: Impossible', 'Brian De Palma', 'ACTION', TRUE, '/movies/mission-impossible.jpg'),
+( 'The Mask', 'Chuck Russell', 'COMEDY', FALSE, '/movies/the-mask.jpg'),
+( 'Superbad', 'Greg Mottola', 'COMEDY', TRUE, '/movies/superbad.jpg'),
+( 'Groundhog Day', 'Harold Ramis', 'COMEDY', FALSE, '/movies/groundhog-day.jpg'),
+( 'Dumb and Dumber', 'Peter Farrelly', 'COMEDY', TRUE, '/movies/dumb-and-dumber.jpg'),
+( 'Home Alone', 'Chris Columbus', 'COMEDY', FALSE, '/movies/home-alone.jpg'),
+( 'The Hangover', 'Todd Phillips', 'COMEDY', TRUE, '/movies/the-hangover.jpg'),
+( 'Mrs. Doubtfire', 'Chris Columbus', 'COMEDY', FALSE, '/movies/mrs-doubtfire.jpg');
